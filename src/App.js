b77d51c89e90cbaf82ee04d26e1cbaeb3289d3f4
dmacisso <dmacisso@gmail.com>
2022-09-15 07:40:05 -0400

    go over props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
+  console.log('We are about to list the employees');
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello Jello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log('Inside the jsx return')}
+      {showEmployees ? (
+        <>
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+        </>
+      ) : (
+        <p>You cannot see the employee list</p>
+      )}
     </div>
   );
 }
---
+++ src/components/Employee.js
@@ -0,0 +1,6 @@
+
+const Employee = () => {
+  return <h3>Here is an employee!!</h3>;
+};
+
+export default Employee;
