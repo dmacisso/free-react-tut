1a548daf586b3853703dabcc2f517f9a9b4f8a75
dmacisso <dmacisso@gmail.com>
2022-10-16 07:52:47 -0400

    add an addCustomer component

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -7,7 +7,7 @@ import NotFound from '../components/NotFound';
 
 export default function Customer() {
   const { id } = useParams();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const [customer, setCustomer] = useState();
   const [notFound, setNotFound] = useState();
@@ -37,6 +37,24 @@ export default function Customer() {
           <p>{customer.industry}</p>
         </div>
       ) : null}
+      <button
+        onClick={(e) => {
+          // const url = `${baseUrl}api/customers/${id}}`;
+          const url = baseUrl + 'api/customers/' + id;
+          fetch(url, { method: 'DELETE', headers: { 'Content-Type': 'application/json'} })
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error('Something went wrong when fetching');
+              }
+              // assume things went well..
+              navigate('/customers');
+            })
+            .catch((err) => console.log(err));
+        }}
+      >
+        Delete
+      </button>
+      <br />
       <Link to="/customers">Customers</Link>
     </>
   );
--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { json, Link } from 'react-router-dom';
 import { baseUrl } from '../shared';
+
+// components
+import AddCustomer from '../components/AddCustomer';
 
 function Customers() {
   const [customers, setCustomers] = useState('');
@@ -10,23 +13,49 @@ function Customers() {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         setCustomers(data.customers);
       });
   }, []);
 
+  function newCustomer(name, industry) {
+    const data = { name: name, industry: industry };
+    const url = baseUrl + 'api/customers/';
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Something went wrong');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // assume add was successful
+        // hide the Modal
+        // make sure the list is updated appropriately
+      })
+      .catch((error) => console.log(error));
+  }
   return (
     <>
-      <h1>Our Customers:</h1>
-      {customers
-        ? customers.map((customer) => {
-            return (
-              <p>
-                <Link to={`/customers/${customer.id}`}>{customer.name}</Link>;
-              </p>
-            );
-          })
-        : null}
+      <ul>
+        <h1>Here are our Customers:</h1>
+        {customers
+          ? customers.map((customer) => {
+              return (
+                <li key={customer.id}>
+                  <Link to={`/customers/${customer.id}`}>{customer.name}</Link>;
+                </li>
+              );
+            })
+          : null}
+      </ul>
+      <AddCustomer newCustomer={newCustomer} />
     </>
   );
 }
